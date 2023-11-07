fe1d68ae5bbe84656a9fb3725e2d659d66f0bd6d
Arkaprabha Banerjee <124279312+Arkaprabha13@users.noreply.github.com>
2023-11-07 21:11:26 +0530

    Create reverse_k_groups.cpp

---
+++ Linked_list/reverse_k_groups.cpp
@@ -0,0 +1,80 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node *next;
+    Node(int x)
+    {
+        data = x;
+        next = NULL;
+    }
+};
+
+void printlist(Node *head)
+{
+    Node *curr = head;
+    while (curr != NULL)
+    {
+        cout << curr->data << " ";
+        curr = curr->next;
+    }
+    cout << endl;
+}
+
+Node *reverseK(Node *head, int k)
+{
+    Node *curr = head, *next = NULL, *prev = NULL;
+    int count = 0;
+    while (curr != NULL && count < k)
+    {
+        next = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = next;
+        count++;
+    }
+    if (next != NULL)
+    {
+        Node *rest_head = reverseK(next, k);
+        head->next = rest_head;
+    }
+    return prev;
+}
+// iteravtive 
+Node *i_reverseK(Node *head,int k){
+    Node *curr=head,*prevFirst=NULL;
+    bool isFirstPass=true;
+    while(curr!=NULL){
+        Node *first=curr,*prev=NULL;
+        int count=0;
+        while(curr!=NULL&&count<k){
+        Node *next=curr->next;
+        curr->next=prev;
+        prev=curr;
+        curr=next;
+        count++;
+        }
+        if(isFirstPass){head=prev;isFirstPass=false;}
+        else{prevFirst->next=prev;}
+        prevFirst=first;
+    }
+    return head;
+}
+
+int main()
+{
+    Node *head = new Node(10);
+    head->next = new Node(20);
+    head->next->next = new Node(30);
+    head->next->next->next = new Node(40);
+    head->next->next->next->next = new Node(50);
+    head->next->next->next->next->next = new Node(60);
+    head->next->next->next->next->next->next = new Node(70);
+    printlist(head);
+    head = i_reverseK(head, 3);
+    printlist(head);
+    return 0;
+}
