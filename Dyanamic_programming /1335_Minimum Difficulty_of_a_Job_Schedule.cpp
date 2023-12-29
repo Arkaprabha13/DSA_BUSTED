017a0ddeefb3ee7cf6a16d96c0b7591fa50f2cf6
Arkaprabha Banerjee <124279312+Arkaprabha13@users.noreply.github.com>
2023-12-29 14:29:16 +0530

    Update 1335_Minimum Difficulty_of_a_Job_Schedule.cpp

--- Dyanamic_programming /1335_Minimum Difficulty_of_a_Job_Schedule.cpp
+++ Dyanamic_programming /1335_Minimum Difficulty_of_a_Job_Schedule.cpp
@@ -1,1 +1,41 @@
+class Solution {
+public:
+    int helper(vector<int>& jobDifficulty, int n, int d, int start, vector<vector<int>>& memo) {
+    if (d == 1) {
+        int maxDifficulty = INT_MIN;
+        for (int i = start; i < n; i++) {
+            maxDifficulty = max(maxDifficulty, jobDifficulty[i]);
+        }
+        return maxDifficulty;
+    }
 
+    if (memo[d][start] != -1) {
+        return memo[d][start];
+    }
+
+    int minDifficulty = INT_MAX;
+    int maxDifficulty = INT_MIN;
+
+    for (int i = start; i < n - d + 1; i++) {
+        maxDifficulty = max(maxDifficulty, jobDifficulty[i]);
+        int nextDifficulty = helper(jobDifficulty, n, d - 1, i + 1, memo);
+        minDifficulty = min(minDifficulty, maxDifficulty + nextDifficulty);
+    }
+
+    memo[d][start] = minDifficulty;
+    return minDifficulty;
+}
+
+int minDifficulty(vector<int>& jobDifficulty, int d) {
+    int n = jobDifficulty.size();
+
+    if (n < d) {
+        return -1;  // Cannot schedule jobs in d days
+    }
+
+    vector<vector<int>> memo(d + 1, vector<int>(n, -1));
+
+    return helper(jobDifficulty, n, d, 0, memo);
+}
+
+};
